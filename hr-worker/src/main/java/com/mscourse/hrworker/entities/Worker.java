70cd48179bc88bd69d9ec6611827f33f58dc9b9a
Antony Rafael <depaulacostaantony@gmail.com>
2020-12-26 19:02:28 -0300

    hr-worker implementations corrections

--- hr-worker/src/main/java/com/mscourse/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/mscourse/hrworker/entities/Worker.java
@@ -11,27 +11,29 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private Double dailyIncome;
-	
-	public Worker(long id, String name, Double dailyIncome) {
+
+	public Worker() {
+	}
+
+	public Worker(Long id, String name, Double dailyIncome) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.dailyIncome = dailyIncome;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -55,7 +57,7 @@ public class Worker implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -68,15 +70,11 @@ public class Worker implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Worker other = (Worker) obj;
-		if (id != other.id)
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
-
 }
--- hr-worker/src/main/java/com/mscourse/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/mscourse/hrworker/repositories/WorkerRepository.java
@@ -1,11 +1,10 @@
 package com.mscourse.hrworker.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mscourse.hrworker.entities.Worker;
 
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-	
-	
-	
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
 }
