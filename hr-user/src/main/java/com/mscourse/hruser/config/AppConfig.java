e1765b1578770816fb21c10fbe0a173266470f43
Antony Rafael <depaulacostaantony@gmail.com>
2020-12-30 21:07:20 -0300

    Database seeding

---
+++ hr-user/src/main/java/com/mscourse/hruser/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.mscourse.hruser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	public BCryptPasswordEncoder passowordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
